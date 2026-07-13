0bc0de40538a77bfe99a49b49e50e797add2fa1c
tanisha0810-dell <singhaltanisha2005@gmail.com>
2026-07-13 10:49:21 +0530

    JumpGame

---
+++ 13July/JumpGame2.java
@@ -0,0 +1,15 @@
+class JumpGame2{
+    public static int jump(int[] nums){
+        int jumps=0;
+        int currEnd=0;
+        int currFarthest=0;
+        for(int i=0;i<nums.length-1;i++){
+            currFarthest=Math.max(currFarthest,i+nums[i]);
+            if(i==currEnd){
+                jumps++;
+                currEnd=currFarthest;
+            }
+        }
+        return jumps;
+    }
+}
